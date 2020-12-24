062b63f5b098bef328635357e25fea6bdccdb195
Md. Jakaria Hossain Jihad <jakariahossen669@gmail.com>
2020-12-24 23:49:28 +0600

    New test project.

---
+++ Task14/src/task14/Shape.java
@@ -0,0 +1,24 @@
+package task14;
+
+public abstract class Shape {
+	// Md. Jakaria Hossain Jihad
+//	2011773642
+
+	private String color;
+	
+	public void getArea() {
+	}
+	
+	public Shape(String color) {
+		this.color=color;
+		
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+
+}
